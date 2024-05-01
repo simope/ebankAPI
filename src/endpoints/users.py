a3e633e819dae009bf658e50ac4ea91929286efd
simope <simonepecora@live.it>
2024-05-01 16:00:48 +0200

    add find user by id and delete user by id

--- src/endpoints/users.py
+++ src/endpoints/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, status, Body
 from typing import List
 from src.models.users import User
-
+from uuid import UUID
 import src.rules.users as users
 
 router = APIRouter(prefix="/users",
@@ -14,3 +14,11 @@ def create_user(request: Request, user: User = Body(...)):
 @router.get("/", response_description="List users", response_model=List[User])
 def list_users(request: Request):
     return users.list_users(request, 100)
+
+@router.get("/{id}", response_description="Get user by id", response_model=User)
+def find_user(request: Request, id: str):
+    return users.find_user(request, id)
+
+@router.delete("/{id}", response_description="Delete a user by id")
+def delete_user(request: Request, id: str):
+    return users.delete_user(request, id)
--- src/rules/users.py
+++ src/rules/users.py
@@ -1,11 +1,11 @@
-from fastapi import Request, Body
+from fastapi import Request, Body, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from src.models.users import User
-
+from bson import ObjectId
+from uuid import UUID
 
 def get_collection_users(request: Request):
     return request.app.database["users"]
-
 
 def create_user(request: Request, user: User = Body(...)):
     user = jsonable_encoder(user)
@@ -16,3 +16,16 @@ def create_user(request: Request, user: User = Body(...)):
 def list_users(request: Request, limit: int):
     users = list(get_collection_users(request).find(limit=limit))
     return users
+
+def find_user(request: Request, id: UUID):
+    if (user := get_collection_users(request).find_one({"_id": id})):
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found.")
+
+def delete_user(request: Request, id: UUID):
+    deleted_user = get_collection_users(request).delete_one({"_id": id})
+
+    if deleted_user.deleted_count == 1:
+        return f"User with id {id} deleted sucessfully."
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found.")
